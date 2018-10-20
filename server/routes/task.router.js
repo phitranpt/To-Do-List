3561d54075c86875c1afb27810e7c3660fe870e6
phitranpt <phi.tran.pt@gmail.com>
2018-10-20 11:02:22 -0500

    added get route on task.route.js

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const pool = require('../modules/database-pool.js');
 
 //GET ROUTE
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM task;`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log('got tasks back from the database', result);
+            res.send(result.rows)
+        })
+        .catch((error) => {
+            console.log(`error in making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        }) 
+})
 
 //POST ROUTE
 
