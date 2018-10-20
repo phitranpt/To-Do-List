4cb405d00fe96b8f76c6ffc3a4cb8438323552b6
phitranpt <phi.tran.pt@gmail.com>
2018-10-20 15:48:51 -0500

    decided to take different approach and deleted routes and modules folder. everything will be housed in server now

--- server/public/client.js
+++ server/public/client.js
@@ -2,6 +2,15 @@ console.log('JS');
 
 $(document).ready(onReady);
 
+//onReady listener
 function onReady() {
     console.log('JQ');
+    $('#addTaskBtn').on('click', addTask);
+    getAllTask();
 }
+
+//takes in users inputs and adds tasks
+function addTask() {
+    console.log('addTaskBtn works!');
+}
+
--- server/server.js
+++ server/server.js
@@ -2,15 +2,38 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const taskRouter = require('./routes/task.router.js');
 
 //uses
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//direct '/task' url request to this file
-app.use('/task', taskRouter);
+//database
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool ({
+    database: 'weekend_to_do_list',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 300000
+})
+
+pool.on('connect', () => {
+    console.log('connected to database');
+})
+
+pool.on('error', (error) => {
+    console.log('error connecting to database', error);
+});
+
+//GET route
+
+//POST route
+
+//DELETE route
+
+//PUT route
 
 //globals
 const port = process.env.PORT || 3000;
